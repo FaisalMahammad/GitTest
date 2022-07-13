ed145b2339be9ff992ae6fd2691e4d598e45202f
FaisalMahammad <misu618@hotmail.com>
2022-07-13 10:56:39 -0400

    First Commit

---
+++ src/com/talentech/gitTest/Greetings.java
@@ -0,0 +1,17 @@
+package com.talentech.gitTest;
+
+public class Greetings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greetings greetings = new Greetings();
+		greetings.greetings();
+	}
+
+	public void greetings(){
+	
+		System.out.println("Faisal says 'Hi Everyone!!!'");
+		
+	}
+
+}
